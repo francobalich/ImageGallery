1ea430dca5ee0c7b74e80b57c8e4a882fc5ca80a
Franco Balich <franco.balich@gmail.com>
2023-06-14 18:23:59 -0300

    Add app

--- src/GalleryApp.jsx
+++ src/GalleryApp.jsx
@@ -1,53 +1,12 @@
 import './GalleryApp.css'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import logoReact from './assets/react.png'
+import { Gallery } from './components/Gallery';
+import { Gallery2 } from './components/Gallery2';
 
 function GalleryApp() {
   return (
     <>
-      <div class="row">
-        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-            class="w-100 shadow-1-strong rounded mb-4"
-            alt="Boat on Calm Water"
-          />
-
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
-            class="w-100 shadow-1-strong rounded mb-4"
-            alt="Wintry Mountain Landscape"
-          />
-        </div>
-
-        <div class="col-lg-4 mb-4 mb-lg-0">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
-            class="w-100 shadow-1-strong rounded mb-4"
-            alt="Mountains in the Clouds"
-          />
-
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-            class="w-100 shadow-1-strong rounded mb-4"
-            alt="Boat on Calm Water"
-          />
-        </div>
-
-        <div class="col-lg-4 mb-4 mb-lg-0">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-            class="w-100 shadow-1-strong rounded mb-4"
-            alt="Waves at Sea"
-          />
-
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
-            class="w-100 shadow-1-strong rounded mb-4"
-            alt="Yosemite National Park"
-          />
-        </div>
-      </div>
+      <Gallery2 />
     </>
   )
 }

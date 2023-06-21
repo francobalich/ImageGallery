72b59a26be477222c67f880f7a6419a862e85bef
Franco Balich <franco.balich@gmail.com>
2023-06-21 20:32:22 -0300

    Fix name class

--- src/components/Gallery2.jsx
+++ src/components/Gallery2.jsx
@@ -23,15 +23,15 @@ export const Gallery2 = () => {
     let cantidadImgs = columns.length / 3
     setImagesData(imagesData =>
       <>
-      <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
-        {columns.slice(0,cantidadImgs)}
-      </div>
-      <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
-        {columns.slice(cantidadImgs,cantidadImgs*2)}
-      </div>
-      <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
-        {columns.slice(cantidadImgs*2,columns.length-1)}
-      </div>
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
+          {columns.slice(0,cantidadImgs)}
+        </div>
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
+          {columns.slice(cantidadImgs,cantidadImgs*2)}
+        </div>
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
+          {columns.slice(cantidadImgs*2,columns.length-1)}
+        </div>
       </>
     )
   }
@@ -45,8 +45,9 @@ export const Gallery2 = () => {
     generateImages()
     //console.log(columnsData);
   }, [columns])
+
   return (
-    <div className="row galleyImage">
+    <div className="row galleryImage">
       {imagesData}
     </div >
   )

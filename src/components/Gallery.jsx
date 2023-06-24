d2cf5c99cebcc6beb9dcb024db4e0fa9c9dee56b
Franco Balich <franco.balich@gmail.com>
2023-06-24 18:00:38 -0300

    Clean project

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,35 +1,54 @@
 import { Image } from './Image';
+import imageData from '../data/images.json'
+import { useEffect, useState } from 'react';
+import { Column } from './Column';
 
 export const Gallery = () => {
+  const [columns, setColumns] = useState([])
+  const [imagesData, setImagesData] = useState(<></>)
+
+  const loadImages = () => {
+    let images = []
+    let i = 0
+    imageData.forEach(data => {
+      images.push(<Image
+        key={i}
+        image={data.src}
+        alt={data.alt} />)
+      i++
+    });
+    return images
+  }
+  const generateImages = () => {
+    let cantidadImgs = columns.length / 3
+    setImagesData(imagesData =>
+      <>
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
+          {columns.slice(0,cantidadImgs)}
+        </div>
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
+          {columns.slice(cantidadImgs,cantidadImgs*2)}
+        </div>
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
+          {columns.slice(cantidadImgs*2,columns.length-1)}
+        </div>
+      </>
+    )
+  }
+  useEffect(() => {
+    const images = loadImages()
+    setColumns(images)
+    //console.log(columnsData);
+  }, [])
+
+  useEffect(() => {
+    generateImages()
+    //console.log(columnsData);
+  }, [columns])
+
   return (
-    <div className="row">
-      <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
-        <Image
-          image="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          alt="Boat on Calm Water" />
-        <Image
-          image="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
-          alt="Wintry Mountain Landscape" />
-      </div>
-
-      <div className="col-lg-4 mb-4 mb-lg-0">
-        <Image
-          image="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
-          alt="Mountains in the Clouds" />
-
-        <Image
-          image="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          alt="Boat on Calm Water" />
-      </div>
-
-      <div className="col-lg-4 mb-4 mb-lg-0">
-        <Image
-          image="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-          alt="Waves at Sea" />
-        <Image
-          image="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
-          alt="Yosemite National Park" />
-      </div>
-    </div>
+    <div className="row galleryImage">
+      {imagesData}
+    </div >
   )
 }

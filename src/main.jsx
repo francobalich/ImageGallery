53d2cb3816685b20b66ae94182c17923b3a8735b
Franco Balich <franco.balich@gmail.com>
2023-06-27 14:12:14 -0300

    Add UserProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import GalleryApp from './GalleryApp.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './context/UserProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GalleryApp />
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <GalleryApp />
+      </BrowserRouter>
+    </UserProvider>
   </React.StrictMode>,
 )

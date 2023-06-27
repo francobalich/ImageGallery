1efb0622138d41012a9e213c23645a8ea4f4a7fd
Franco Balich <franco.balich@gmail.com>
2023-06-27 13:13:49 -0300

    Add login submit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,13 +5,43 @@ import {
   MDBCheckbox,
   MDBBtn
 } from 'mdb-react-ui-kit';
+import { useForm } from '../hooks/useForm';
+
+const loginFormFields = {
+  loginEmail: '',
+  loginPassword: ''
+}
 
 export const LoginPage = () => {
+  const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm(loginFormFields)
+  const loginSubmit = (event) => {
+    event.preventDefault()
+    console.log({
+        email: loginEmail,
+        password: loginPassword
+    })
+  }
   return (
     <section className='pageContainer__login mainPage'>
-      <form className='loginForm'>
-        <MDBInput className='mb-4 fondoBlanco' type='email' id='form1Example1' label='Ingrese su mail' />
-        <MDBInput className='mb-4 fondoBlanco' type='password' id='form1Example2' label='Ingrese su contraseña' />
+      <form className='loginForm' onSubmit={loginSubmit}>
+        <MDBInput
+          className='mb-4 fondoBlanco'
+          type='email'
+          id='form1Example1'
+          label='Ingrese su mail'
+          name='loginEmail'
+          value={loginEmail}
+          onChange={onLoginInputChange}
+        />
+        <MDBInput
+          className='mb-4 fondoBlanco'
+          type='password'
+          id='form1Example2'
+          label='Ingrese su contraseña'
+          name='loginPassword'
+          value={loginPassword}
+          onChange={onLoginInputChange}
+        />
         <MDBBtn type='submit' block>
           Iniciar Sesión
         </MDBBtn>

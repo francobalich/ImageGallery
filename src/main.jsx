0bebd741447a4158a560abbbb438088c230a07e9
Franco Balich <franco.balich@gmail.com>
2023-06-26 22:55:58 -0300

    Add BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GalleryApp from './GalleryApp.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GalleryApp />
+    <BrowserRouter>
+      <GalleryApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )

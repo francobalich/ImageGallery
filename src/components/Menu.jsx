3a9a90c18df11b6434f632fa3c3f47aae0d93e28
Franco Balich <franco.balich@gmail.com>
2023-06-28 20:17:07 -0300

    Add menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBIcon,
+  MDBCollapse
+} from 'mdb-react-ui-kit';
 
 export const Menu = () => {
+  const [showNavSecond, setShowNavSecond] = useState(false);
+
   return (
-    <header>
-      <nav>
-        <Link to='/'>Gallery</Link>
-        <Link to='/auth/login'> Iniciar Sesión</Link>
-        <Link to='/auth/register'>Registrarse</Link>
-      </nav>
-    </header>
-  )
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>ImageGallery</MDBNavbarBrand>
+        <MDBNavbarToggler
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavSecond(!showNavSecond)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar show={showNavSecond}>
+          <MDBNavbarNav>
+            <Link to='/'>
+              <MDBNavbarLink active aria-current='page' href='#'>Gallery</MDBNavbarLink> 
+            </Link>
+            <Link to='/auth/register'>
+              <MDBNavbarLink href='#'>Registrarse</MDBNavbarLink>
+            </Link>
+            <Link to='/auth/login'>
+              <MDBNavbarLink href='#'>Iniciar Sesión</MDBNavbarLink> 
+            </Link>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
 }

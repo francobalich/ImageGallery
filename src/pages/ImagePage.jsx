2aac754ab90f6d55bb255ff4fe141a2df15321ec
Franco Balich <franco.balich@gmail.com>
2023-07-11 14:31:59 -0300

    Feat: Add image in image page

--- src/pages/ImagePage.jsx
+++ src/pages/ImagePage.jsx
@@ -1,17 +1,24 @@
-import React, { useContext, useEffect } from 'react'
-import { Gallery } from '../components/Gallery'
-import { Menu } from '../components/Menu'
+import React, { useContext, useEffect, useState } from "react";
+import { Gallery } from "../components/Gallery";
+import { Menu } from "../components/Menu";
+import { useParams } from "react-router-dom";
+import imageData from "../data/images.json";
+import { Image } from "../components/Image";
 
 export const ImagePage = () => {
-
+  const params = useParams();
+  const [img, setImg] = useState({});
   useEffect(() => {
-    console.log('user');
-  }, [])
+    const img = imageData.filter((x) => x.id === params.id)[0];
+    setImg(img);
+  }, []);
   return (
     <>
-    <Menu />
-    {/* <Image image="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" alt="imagende prueba" /> */}
-    <h1>Hola mundo</h1>
+      <Menu />
+      <section className="imgSection">
+        <h1>Imagen id: {img.id}</h1>
+        <Image id={img.id} image={img.src} alt={img.alt} />
+      </section>
     </>
-  )
-}
+  );
+};

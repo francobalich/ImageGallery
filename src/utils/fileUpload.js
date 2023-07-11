e882d858539515c6a133d50c2d9ef7067a28b4f7
Franco Balich <franco.balich@gmail.com>
2023-07-11 14:02:46 -0300

    Fix: Update format

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -1,20 +1,20 @@
-export const fileUpload = async(file)=>{
-    if(!file) throw new Error('No tenemos ningun archivo a subir')
-    if(!file) return null
+export const fileUpload = async (file) => {
+    if (!file) throw new Error('No tenemos ningun archivo a subir')
+    if (!file) return null
 
     const cloudUrl = `https://api.cloudinary.com/v1_1/dzmkef9sr/upload`
 
     const formData = new FormData()
-    formData.append('upload_preset','react-journal')
-    formData.append('file',file)
+    formData.append('upload_preset', 'react-journal')
+    formData.append('file', file)
 
     try {
-        const resp = await fetch(cloudUrl,{
-            method:'POST',
-            body:formData
+        const resp = await fetch(cloudUrl, {
+            method: 'POST',
+            body: formData
         })
 
-        if(!resp.ok) throw new Error('No se pudo subir la imagen')
+        if (!resp.ok) throw new Error('No se pudo subir la imagen')
         const cloudResp = await resp.json()
 
         return cloudResp.secure_url

82aa5e804c2b11024052cb61d51b5a4489af81b6
Franco Balich <franco.balich@gmail.com>
2023-07-11 15:01:58 -0300

    Change: upload_preset

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -5,7 +5,7 @@ export const fileUpload = async (file) => {
     const cloudUrl = `https://api.cloudinary.com/v1_1/dzmkef9sr/upload`
 
     const formData = new FormData()
-    formData.append('upload_preset', 'react-journal')
+    formData.append('upload_preset', 'curso-react')
     formData.append('file', file)
 
     try {

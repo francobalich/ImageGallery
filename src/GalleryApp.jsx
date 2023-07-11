2265b5050b458bf5897c2ebe113b5db53fdd6203
Franco Balich <franco.balich@gmail.com>
2023-07-11 20:56:28 -0300

    Change: Update css

--- src/GalleryApp.jsx
+++ src/GalleryApp.jsx
@@ -1,4 +1,3 @@
-import './GalleryApp.css'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { LoginPage } from './pages/LoginPage';
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GalleryApp from './GalleryApp.jsx'
-import './index.css'
+import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'

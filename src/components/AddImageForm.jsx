95d6975318b667edb326bd7d340aa06c801abcb7
Franco Balich <franco.balich@gmail.com>
2023-07-11 18:26:25 -0300

    Feat: Send json to put images

--- src/components/AddImageForm.jsx
+++ src/components/AddImageForm.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
-import { getAllImages, startUploadFiles } from '../utils/fileManager';
+import { getAllImages, saveImages, startUploadFiles } from '../utils/fileManager';
 import { useAuthStore } from '../hooks/useAuthStore';
 import '../styles/AddImageForm.css'
 import { MDBBtn, MDBInput } from 'mdb-react-ui-kit';
@@ -12,69 +12,72 @@ const imgDataFormFields = {
 const formValidations = {
   imgDataTitle: [(value) => value.length >= 1, 'El titulo es obligatorio'],
 }
-export const AddImageForm = ({status, setStatus}) => {
+export const AddImageForm = ({ status, setStatus, setImageList }) => {
   const { user } = useAuthStore()
   const [formSubmitted, setFormSubmitted] = useState(false)
+  const [path, setPath] = useState()
+  const fileInputRef = useRef();
+
   const { imgDataTitle, imgDataTitleValid, onInputChange: onImgInputChange } = useForm(imgDataFormFields, formValidations)
 
   const onFileInputChange = async ({ target }) => {
     if (target.files === 0) return;
+    setPath(target.files)
+  }
 
-    const path = await startUploadFiles(target.files)
-    console.log(path);
-  };
-  const onSubmitData = (event) => {
+  const onSubmitData = async(event) => {
     event.preventDefault()
     setFormSubmitted(true)
-    console.log("Se envio el form");
+    const url = await startUploadFiles(path)
+    const images=await getAllImages(user.email)
+    setImageList(images)
+    setStatus(false)
+    saveImages(user.email,imgDataTitle,url)
   }
-  const onCancel = ()=>{
+
+  const onCancel = () => {
     setStatus(false)
   }
-  useEffect(() => {
-    getAllImages(user.email)
-  }, [])
 
-  const fileInputRef = useRef();
   return (
-    (status)?(
-    <section className='formAlert'>
-      <form className='loginForm' onSubmit={onSubmitData}>
-        <InputLabel text="Ingrese el titulo de la imagen" state={!!imgDataTitleValid && formSubmitted} />
-        <MDBInput
-          className='mb-4 fondoBlanco'
-          type='text'
-          label='Ingrese el titulo de la imagen'
-          name='imgDataTitle'
-          value={imgDataTitle}
-          onChange={onImgInputChange}
-        />
-        <div>
-          <input
-            type="file"
-            multiple
-            ref={fileInputRef}
-            onChange={onFileInputChange}
-            style={{ display: "none" }}
+    (status) ? (
+      <section className='formAlert'>
+        <form className='loginForm' onSubmit={onSubmitData}>
+          <InputLabel text="Ingrese el titulo de la imagen" state={!!imgDataTitleValid && formSubmitted} />
+          <MDBInput
+            className='mb-4 fondoBlanco'
+            type='text'
+            label='Ingrese el titulo de la imagen'
+            name='imgDataTitle'
+            value={imgDataTitle}
+            onChange={onImgInputChange}
           />
-          <MDBBtn type='button'
-            className='mb-4'
-            onClick={() => {
-              fileInputRef.current.click();
-            }}
-            block>
-            Haga clic aca para subir una imagen
+          <div>
+            <input
+              type="file"
+              multiple
+              ref={fileInputRef}
+              onChange={onFileInputChange}
+              style={{ display: "none" }}
+            />
+            <MDBBtn type='button'
+              className='mb-4'
+              onClick={() => {
+                fileInputRef.current.click();
+              }}
+              block>
+              Haga clic aca para subir una imagen
+            </MDBBtn>
+          </div>
+          <MDBBtn type='submit' color='success' className='mb-4' block>
+            Guardar Imagen
           </MDBBtn>
-        </div>
-        <MDBBtn type='submit' color='success' className='mb-4' block>
-          Guardar Imagen
-        </MDBBtn>
-        <MDBBtn type='button' color='danger' className='mb-4' block onClick={onCancel}>
-          Cerrar
-        </MDBBtn>
-      </form>
-    </section>
-    ):
-    <></>
+          <MDBBtn type='button' color='danger' className='mb-4' block onClick={onCancel}>
+            Cerrar
+          </MDBBtn>
+        </form>
+      </section>
+    ) :
+      <></>
   )
 }

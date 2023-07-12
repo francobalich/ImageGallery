3d551f2fe755de5429e257865bd3790513c5c6cf
Franco Balich <franco.balich@gmail.com>
2023-07-12 20:31:15 -0300

    Change: Add new comment

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,5 +1,6 @@
 // Configuración de axios para que todas las APIs utilicen el mismo URL
 // Y que los Headers usen el Header x-token en donde estara el JWT
+// Para instalar axios tienen que usar npm install axios
 import axios from 'axios'
 
 const API_URL = "https://api.francobalich.com/api"

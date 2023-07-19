693ade37113d37c068f9a189664899ebc888a4a6
Franco Balich <franco.balich@gmail.com>
2023-07-19 18:10:16 -0300

    Feat: Add test in InputLabels

---
+++ test/components/InputLabel.test.jsx
@@ -0,0 +1,9 @@
+import { render,screen } from "@testing-library/react";
+import { InputLabel } from "../../src/components/InputLabel";
+
+describe('Pruebas en <InputLabel />', () => {
+    test('Debe de mostrar un InputLabel con el texto "test"', () => {
+        render( <InputLabel text="test" state={false} /> )
+        expect( screen.getByText('test') ).toBeTruthy()
+    })
+})

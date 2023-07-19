5c8c338fb13a3b92abf6d0c4b0e9755ab06cba07
Franco Balich <franco.balich@gmail.com>
2023-07-19 17:45:55 -0300

    Feat: Add test in useAuthStore

---
+++ test/hooks/useAuthStore.test.js
@@ -0,0 +1,56 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { act, renderHook, waitFor } from "@testing-library/react"
+import { Provider } from "react-redux"
+
+import { notAuthenticatedState } from "../fixtures/authStates"
+import { testUserCredentials } from "../fixtures/testUser"
+import { useAuthStore } from "../../src/hooks/useAuthStore"
+import { authSlice } from "../../src/store/auth/authSlice"
+
+const getMockStore = (initialState) => {
+    return configureStore({
+        reducer: {
+            auth: authSlice.reducer
+        },
+        preloadedState: {
+            auth: { ...initialState }
+        }
+    })
+}
+describe('Pruebas en useAuthStore', () => {
+    beforeEach(()=>localStorage.clear())
+    test('startLogin debe de realizar el login correctamente', async() => {
+        const mockStore = getMockStore({ ...notAuthenticatedState })
+        const { result } = renderHook(() => useAuthStore(), {
+            wrapper: ({ children }) => <Provider store={mockStore}>{children}</Provider>
+        })
+        await act(async()=>{
+            await result.current.startLogin({...testUserCredentials})
+        })
+        const {errorMessage, status, user}=result.current;
+        expect({errorMessage, status, user}).toEqual({
+            errorMessage: undefined,
+            status: 'authenticated',
+            user: expect.any(Object)
+        })
+    })
+    test('startLogin debe de fallar la autenticacion', async() => {
+        const mockStore = getMockStore({ ...notAuthenticatedState })
+        const { result } = renderHook(() => useAuthStore(), {
+            wrapper: ({ children }) => <Provider store={mockStore}>{children}</Provider>
+        })
+        await act(async()=>{
+            await result.current.startLogin({email:"test@gmail.com",password:"456789"})
+        })
+        const {errorMessage, status, user}=result.current;
+        expect({errorMessage, status, user}).toEqual({
+            errorMessage: expect.any(String),
+            status: 'not-authenticated',
+            user: {}
+        })
+        expect(localStorage.getItem('token')).toBe(null)
+        waitFor(
+            ()=>expect(result.current.errorMessage).toBe(undefined)
+        )
+    })
+})

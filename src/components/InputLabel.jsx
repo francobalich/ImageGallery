d26fe2713503a15ded472465e22abff27c1ea546
Franco Balich <franco.balich@gmail.com>
2023-07-26 20:19:42 -0300

    Feat: Add proptypes

--- src/components/InputLabel.jsx
+++ src/components/InputLabel.jsx
@@ -1,8 +1,14 @@
+import PropTypes from 'prop-types'
 // Componente para mostrar campos obligatorios en el formulario de inicio de sesión
+
 export const InputLabel = ({ text, state }) => {
   return (
     <>
       <p className={`login__inputTitle ${(state) ? "red" : ""}`} ><span>*</span>{text}</p>
     </>
   )
+}
+InputLabel.propTypes={
+  text:PropTypes.string.isRequired,
+  state:PropTypes.bool.isRequired,
 }

a8a018209ae96fc40a7ff5c7f5b73619a51c59be
Franco Balich <franco.balich@gmail.com>
2023-07-26 19:31:28 -0300

    Feat: Add propTypes

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import reactLogo from "../assets/react.png";
+import PropTypes from 'prop-types'
 
 export const Image = ({id=0, image = reactLogo, alt = "imagen" }) => {
   return (
@@ -12,3 +13,8 @@ export const Image = ({id=0, image = reactLogo, alt = "imagen" }) => {
     </Link>
   );
 };
+Image.propTypes={
+  id:PropTypes.number.isRequired,
+  image:PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  alt:PropTypes.string,
+}
